0524cdd33c8ff2396f0ba0edbecca5b8232e2995
zhiliang <yotoobo@users.noreply.github.com>
2014-09-17 07:53:13 +0800

    Update fedroa.sh

--- fedroa.sh
+++ fedroa.sh
@@ -50,3 +50,9 @@ fc-cache -fv
 sudo wget http://download.opensuse.org/repositories/home:/snwh:/orchis-gtk-theme/Fedora_20/home:snwh:orchis-gtk-theme.repo -O /etc/yum.repos.d/orchis-gtk-theme.repo
 sudo yum update
 sudo yum install orchis-gtk-theme
+
+#vim
+cd vim74
+./configure --prefix=/usr/local/ --with-features=huge --enable-multibyte --enable-cscope --enable-sniff --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-python3interp --with-python3-config-dir=/usr/lib/python3.3/config --enable-rubyinterp --enable-perlinterp --enable-luainterp
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim74
+sudo make install
