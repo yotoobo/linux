fc661ea27aa0014af5f1bcb9855bc71b66e03c8d
zhiliang <yotoobo@users.noreply.github.com>
2014-09-17 07:53:26 +0800

    Delete install_vim.sh

--- install_vim.sh
+++
@@ -1,4 +0,0 @@
-cd vim74
-./configure --prefix=/usr/local/ --with-features=huge --enable-multibyte --enable-cscope --enable-sniff --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-python3interp --with-python3-config-dir=/usr/lib/python3.3/config --enable-rubyinterp --enable-perlinterp --enable-luainterp 
-make VIMRUNTIMEDIR=/usr/local/share/vim/vim74
-sudo make install
