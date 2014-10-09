6ac4f39d2b9a76562af0ac8725f84bf30ecbc5a2
zhiliang <yotoobo@users.noreply.github.com>
2014-10-09 10:44:23 +0800

    Update install.php

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 #yum
-yum -y install 
+yum -y install libicu-devel
 
 #download php5.5
 cd ~
@@ -16,3 +16,25 @@ cp sapi/fpm/init.d.php-fpm /etc/init.d/php-fpm
 chmod u+x /etc/init.d/php-fpm
 cp /usr/local/php55/etc/php-fpm.conf.default /usr/local/php55/etc/php-fpm.conf
 [ -d /usr/local/php55/etc/php.d ] || mkdir /usr/local/php55/etc/php.d
+
+#install extension
+cd ~
+wget http://pecl.php.net/get/intl-3.0.0.tgz
+tar zxf intl-3.0.0.tgz
+cd intl-3.0.0
+phpize
+./configure --enable-intl
+make && make install
+echo "extension=intl.so" >> /usr/local/php5/etc/php.d/extension.ini
+cd ..
+
+wget http://pecl.php.net/get/memcache-3.0.8.tgz
+tar zxf memcache-3.0.8.tgz
+cd memcache-3.0.8
+phpize
+./configure --enable-memcache
+make && make install
+echo "extension=memcache.so" >> /usr/local/php5/etc/php.d/extension.ini
+cd ..
+
+
