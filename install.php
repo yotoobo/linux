fa91518c14c7aed144e4db83aad47ca28acc6291
zhiliang <yotoobo@users.noreply.github.com>
2014-10-18 00:32:48 +0800

    Update install.php

--- install.php
+++ install.php
@@ -9,9 +9,9 @@ wget http://mirrors.sohu.com/php/php-5.5.17.tar.bz2
 tar jxf php-5.5.17.tar.bz2
 cd php-5.5.17
 ./configure --prefix=/usr/local/php55 --with-config-file-path=/usr/local/php55/etc \
---with-config-file-scan-dir=/usr/local/php55/etc/php.d --enable-bcmath --enable-exif \
+--with-config-file-scan-dir=/usr/local/php55/etc/php.d --enable-bcmath --enable-exif --enable-sockets \
 --enable-sockets --enable-mbstring --enable-fpm --enable-soap --with-mcrypt --with-bz2 \
---with-openssl --with-zlib --with-mhash --with-mysqli --with-pdo-mysql --with-curl
+--with-openssl --with-zlib --with-mhash --with-mysqli --with-pdo-mysql --with-curl --with-gettext
 make
 make install
 cp php.ini-production /usr/local/php55/etc/php.ini
